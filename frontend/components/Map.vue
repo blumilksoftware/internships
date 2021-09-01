f21c25d375cab6c230c37b25a8fc25efba54be77
brzozka <brzozka108@gmail.com>
2021-09-01 15:42:37 +0200

    #104 Markers function
    Still not visible, need to be fixed

--- frontend/components/Map.vue
+++ frontend/components/Map.vue
@@ -1,3 +1,4 @@
+p
 <template>
   <div>
     <div
@@ -27,6 +28,7 @@ import mapboxgl from "mapbox-gl";
 import { isMobile } from "@/functions/functions";
 import SearchBar from "@/components/SearchBar";
 import { LocationMarkerIcon } from "@heroicons/vue/outline";
+import { mapState, mapGetters } from "vuex";
 
 export default {
   components: {
@@ -34,6 +36,7 @@ export default {
     SearchBar,
   },
   mounted() {
+    this.companiesTest = this.getCompaniesMerged;
     mapboxgl.accessToken = process.env.VUE_APP_MAPBOX_TOKEN;
     this.buildMap();
   },
@@ -43,7 +46,13 @@ export default {
       map: null,
       filtered: [],
       isMobile,
+      markers: [],
+      companiesTest: [],
     };
+  },
+  computed: {
+    ...mapState(["companiesMerged"]),
+    ...mapGetters(["getCompaniesMerged"]),
   },
 
   methods: {
@@ -52,7 +61,53 @@ export default {
         container: "map",
         style: process.env.VUE_APP_MAPBOX_STYLE_URL,
       });
-      this.map.addControl(new mapboxgl.NavigationControl());
+      this.map.on("load", () => {
+        this.map.addControl(new mapboxgl.NavigationControl());
+        console.log(this.companiesTest);
+        if (this.markers.length > 0) {
+          this.resetMarkers();
+        }
+
+        if (this.companiesTest) {
+          this.updateMarkers();
+        }
+        console.log(this.markers);
+      });
+    },
+    resetMarkers() {
+      this.markers.forEach((marker) => {
+        marker.remove();
+      });
+      this.markers = [];
+      console.log("reset");
+    },
+    updateMarkers() {
+      this.markers = this.companiesTest.map((company) => {
+        const el = document.createElement("div");
+        el.className = "marker";
+        const popup = this.companyPopup(company);
+        return new mapboxgl.Marker(el)
+          .setLngLat([
+            company.address.coordinates.longitude,
+            company.address.coordinates.latitude,
+          ])
+          .setPopup(popup)
+          .addTo(this.map);
+      });
+      console.log("update");
+    },
+    companyPopup(company) {
+      return new mapboxgl.Popup({ offset: 25 }).setHTML(`
+      <div class="card">
+        <div class="card-body">
+        <p class="comapny-name font-weight-bold">${company.name}</p>
+        <p class="company-city>${company.cityName}</p>
+
+
+
+        </div>
+      </div>
+      `);
     },
   },
 };
@@ -69,6 +124,26 @@ export default {
 }
 .map-marker-popup > div:not(:first-child) {
   padding: 4px 8px;
+}
+.mapboxgl-marker {
+  width: 25px;
+  height: 25px;
+  border-radius: 50%;
+  border: 1px solid gray;
+  background-color: lightblue;
+}
+.marker {
+  content: "";
+  cursor: pointer;
+  position: absolute;
+  width: 25px;
+  height: 25px;
+  border: 1px solid #ccc;
+  border-radius: 75% 45% 75% 0%;
+  background: #3498db;
+  bottom: 0;
+  transform-origin: 0% 100%;
+  transform: rotate(-45deg) scale(1);
 }
 #map-controls {
   i {
--- frontend/mixins/markers.js
+++ frontend/mixins/markers.js
@@ -1,30 +1,35 @@
-// function buildLayer(result, layer, image) {
-//   return {
-//     id: layer,
-//     type: "symbol",
-//     source: {
-//       type: "geojson",
-//       data: result,
-//     },
-//     layout: {
-//       "icon-image": image,
-//       "icon-allow-overlap": true,
-//       "icon-ignore-placement": true,
-//       "icon-anchor": "bottom",
-//     },
-//   };
-// }
-// const layer = { name: "companies", icon: "marker", offset: [12, -16] };
+function buildLayer(result, layer, image) {
+  return {
+    id: layer,
+    type: "symbol",
+    source: {
+      type: "geojson",
+      data: result,
+    },
+    layout: {
+      "icon-image": image,
+      "icon-allow-overlap": true,
+      "icon-ignore-placement": true,
+      "icon-anchor": "bottom",
+    },
+  };
+}
+const layer = { name: "companies", icon: "marker", offset: [12, -16] };
 
-// export default {
-//   methods: {
-//     buildMarkers() {
-//       this.map.on("load", async () => {
-//         await this.map.loadImage(require('@/assets/marker.png'), (error, image) => {
-//             if (error) {
-//               console.log('Image not loaded.')
-//             }
-//             this.map.addImage('marker', image)
-//       });
-//     }
-//   }
+export default {
+  methods: {
+    buildMarkers() {
+      this.map.on("load", async () => {
+        await this.map.loadImage(
+          require("@/assets/marker.png"),
+          (error, image) => {
+            if (error) {
+              console.log("Image not loaded.");
+            }
+            this.map.addImage("marker", image);
+          }
+        );
+      });
+    },
+  },
+};
